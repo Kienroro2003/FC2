9dcc44f9b05c12b96676c9a68a5ffa6c64604de7
Kienroro2003 <91589673+Kienroro2003@users.noreply.github.com>
2023-03-21 15:14:33 +0700

    udpate

---
+++ src/list/Main.java
@@ -0,0 +1,33 @@
+package java_core.list;
+
+import java.util.LinkedList;
+
+public class Main {
+    public static void main(String[] args) {
+        MyLinkedList<String> stringMyLinkedList = new MyLinkedList<>();
+        stringMyLinkedList.addFirst("Kien");
+        stringMyLinkedList.addFirst("Start");
+        for (int i = 5; i >= 1; i--) {
+            stringMyLinkedList.addLast("Kien" + i);
+        }
+//        stringMyLinkedList.add(0,"Kien111");
+//        stringMyLinkedList.clear();
+//        stringMyLinkedList.addLast("Kien");
+//        stringMyLinkedList.removeLast();
+        System.out.println(stringMyLinkedList);
+        System.out.println("Size: " + stringMyLinkedList.size());
+        System.out.println("indexOf: " + stringMyLinkedList.indexOf("Kien"));
+        System.out.println("lastIndexOf: " + stringMyLinkedList.lastIndexOf("Kien"));
+
+
+
+        LinkedList<String> strings = new LinkedList<>();
+//        System.out.println(strings.removeFirst());
+        for (int i = 0; i < 5; i++) {
+            strings.add("Hello " + i);
+        }
+//        strings.add(7,"addIndex");
+        System.out.println(strings);
+//        System.out.println(strings.get(4));
+    }
+}
---
+++ src/list/MyList.java
@@ -0,0 +1,30 @@
+package java_core.list;
+
+public interface MyList<E> extends Iterable<E> {
+    void add(E e);
+
+    void add(int index, E e);
+
+    void clear();
+
+    boolean contains(E e);
+
+    E get(int index);
+
+    int indexOf(E e);
+
+    int indexOf(E e, int from);
+
+    int lastIndexOf(E e);
+
+    boolean isEmpty();
+
+    boolean remove(E e);
+
+    E remove(int index);
+
+    Object set(int index, E e);
+
+    int size();
+
+}
